041d739d62086246f07bb77aee535698713b0cdc
Yanick Champoux <yanick@babyl.dyndns.org>
2012-10-13 19:26:51 -0400

    Overloaded by app logger
    But the module defaults are replaced by the main logger, if it's set
    
        $ perl -Ilib app.pl
        [22141] foo'ing around
        [22141] doing our best, bar none

--- app.pl
+++ app.pl
@@ -6,7 +6,14 @@ use warnings;
 use Foo;
 use Bar;
 
+use Log::Dispatchouli;
 use Log::Contextual qw/ set_logger /;
+
+set_logger( Log::Dispatchouli->new({
+    ident     => 'myapp',
+    to_stdout => 1,
+    debug     => 1,
+}));
 
 Foo::foo();
 Bar::bar();

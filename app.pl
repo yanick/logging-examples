e54070d4d0bbfce4b3ea17c654cec8307f1e0fac
Yanick Champoux <yanick@babyl.dyndns.org>
2012-10-13 19:18:23 -0400

    Making appl.pl work
    We initialize the logger at the app level, and suddenly, it works!
    
        $ perl -Ilib app.pl
        [info] foo'ing around
        [debug] doing our best, bar none

--- app.pl
+++ app.pl
@@ -6,6 +6,13 @@ use warnings;
 use Foo;
 use Bar;
 
+use Log::Contextual qw/ set_logger /;
+use Log::Contextual::SimpleLogger;
+
+set_logger( Log::Contextual::SimpleLogger->new({
+    levels => [ qw/ debug info / ],
+}) );
+
 Foo::foo();
 Bar::bar();
 

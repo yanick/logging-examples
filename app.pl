c1cc6548b13fd883c0fee8f4d0da052666a7ee43
Yanick Champoux <yanick@babyl.dyndns.org>
2012-10-13 19:37:50 -0400

    Hooking Log::Dispatchouli to the app
    And suddenly, logging happens!
    
        $ perl -Ilib app.pl
        [23624] foo'ing around
        [23624] doing our best, bar none

--- app.pl
+++ app.pl
@@ -6,6 +6,18 @@ use warnings;
 use Foo;
 use Bar;
 
+use Log::Dispatchouli;
+use Log::Any::Adapter;
+
+# Send all logs to Log::Dispatch
+my $log = Log::Dispatchouli->new({
+    ident => 'myapp',
+    debug => 1,
+    to_stdout => 1,
+});
+Log::Any::Adapter->set( 'Dispatch', dispatcher => $log );
+
+
 Foo::foo();
 Bar::bar();
 
